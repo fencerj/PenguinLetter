12727ecd7b161c1a8f617d9dfffde6fef03ac9e8
ivan_Office_MacPro <fencer12@126.com>
2013-05-09 11:25:13 +0800

    Initial Commit

---
+++ PenguinLetter/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  PenguinLetter
+//
+//  Created by Ivan on 13-5-9.
+//  Copyright Ivan 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
